6e493369ab31297e789cf608c56cb4b30a2591f7
unknown <nguyenbacuong.neu@gmail.com>
2025-05-02 13:13:17 +0700

    day-26

---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+    testDir: './tests',
+    projects: [
+        {
+            name: 'Chromium',
+            use: { ...devices['Desktop Chrome'] }
+        },
+        // {
+        //     name: 'Firefox',
+        //     use: {...devices['Desktop Firefox']}
+        // }
+    ],
+    use: {
+        headless: false,
+    }
+})
